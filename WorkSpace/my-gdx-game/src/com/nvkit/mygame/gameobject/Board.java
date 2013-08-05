af2caed6d09488c17164937a03e30c7abc9e9a3b
ngvankhoi <ngvankhoi@gmail.com>
2013-08-05 16:12:52 +0700

    tate

--- WorkSpace/my-gdx-game/src/com/nvkit/mygame/gameobject/Board.java
+++ WorkSpace/my-gdx-game/src/com/nvkit/mygame/gameobject/Board.java
@@ -6,9 +6,18 @@
 
 package com.nvkit.mygame.gameobject;
 
+import java.lang.annotation.*;
+import java.awt.Point;
 import java.util.*;
 import com.nvkit.mygame.concept.IBoard;
 
 /** @pdOid 0c7554ec-d8cd-4063-b375-9b0f78ed73d4 */
 public class Board extends com.badlogic.gdx.graphics.g2d.SpriteBatch implements com.nvkit.mygame.concept.IBoard {
+   /** @pdOid b307023c-d0dd-4263-af6a-0fda04dce6df */
+   @Override
+   public Point getLocation() {
+   	// TODO Auto-generated method stub
+   	return null;
+   }
+
 }
